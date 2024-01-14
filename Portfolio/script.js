b229e5d85641e4f07d45b1af89c528e5b222b4de
sania2715 <146361748+sania2715@users.noreply.github.com>
2024-01-14 23:17:13 +0530

    Create script.js

---
+++ Portfolio/script.js
@@ -0,0 +1,15 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+
+  var tablink = document.getElementsByClassName("tab-links");
+  var tabcontents = document.getElementsByClassName("tab-contents");
+
+  function opentab(tabname){
+    for(tablink of tablinks){
+      tablink.classList.remove("active-link")
+    }
+  }
